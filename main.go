99908def091239c34516175ebd1f6a6576aa91a4
Leon Jalfon <leonjalfon1@gmail.com>
2020-01-19 23:15:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Wololo!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
